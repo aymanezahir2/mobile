2fa5ab9e6c9786a72e723755214a763ab933de48
aymane <aymanefront20042@gmail.com>
2024-02-18 06:06:34 +0100

    first step

--- src/constants/env.ts
+++
@@ -1,3 +0,0 @@
-export const URL_SERVER: string = "http://192.168.11.107:2004"
-export const URL_CLIENT: string = "https://ntbijoux.com/"
-export const REFRECH_TOKEN: string = "07632839de116323cb043149177e799a61aa49dbe59e161cd0a8c733671988fcb219c54ad8c35db20988c92567a4b32d20ae6e5524bab5d99daa1c6ea098e079";
